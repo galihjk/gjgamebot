75253f3e9fa2742638f6c7235772e2d6ca381fc2
Galih Jaya Kusumah <galih.kusumah@bpk.go.id>
2023-01-17 06:44:12 +0700

    bugfix

--- functions/handle_message_start.php
+++ functions/handle_message_start.php
@@ -6,11 +6,10 @@ function handle_message_start($botdata){
         $chat = $botdata["chat"];
         $chat_id = $chat["id"];
 
-        $datakirim = f("pesan_utama")(
-            ["chat_id"=>$chat_id],
-            $botdata["from"]["id"]
-        );
-        f("bot_kirim_perintah")("sendMessage",$datakirim);
+        f("bot_kirim_perintah")("sendMessage",[
+            "chat_id"=>$chat_id,
+            "text"=>"test",
+        ]);
 
         return true;
     }
