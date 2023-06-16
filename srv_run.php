f8ff5867bc4a55c3d6d159592ffb953b575eda54
Galih Jaya Kusumah <galih.kusumah@bpk.go.id>
2023-06-16 15:15:31 +0700

    srv run

--- srv_run.php
+++
@@ -1,4 +0,0 @@
-<?php
-include("init.php");
-f("srv.get_without_wait")("http://galihjk.my.id/gjgamebot/srv_run_fdb8e0f27050d87f4faad843c4da5f3f.php");
-echo "Server STARTED";
--- srv_start.php
+++ srv_start.php
@@ -1,8 +1,8 @@
 <?php
 include("init.php");
-$srvcode = md5(date("mdHis").rand(0,100));
-f("data_save")("servercode", $srvcode);
 f("bot_kirim_perintah")("sendMessage",[
     "chat_id"=>f("get_config")('bot_admins')[0],
-    "text"=>"Server Stopped",
+    "text"=>"Server Started",
 ]);
+f("srv.get_without_wait")(f("get_config")("run_srv"));
+echo "Server STARTED";
