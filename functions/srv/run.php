73a964254e9cd126b5676b0169d6c7fa6f918484
Galih Jaya Kusumah <galih.kusumah@bpk.go.id>
2023-06-16 13:16:19 +0700

    detik berlalu

--- functions/srv/run.php
+++ functions/srv/run.php
@@ -1,8 +1,21 @@
 <?php
     function srv__run(){
         $loopcount = 0;
+        $time1 = time();
+        $time_notif = time()+15;
+        f("bot_kirim_perintah")("sendMessage",[
+            "chat_id"=>f("get_config")('bot_admins')[0],
+            "text"=>"Server Started",
+        ]);
         while(true){
             $loopcount++;
+            if(time() >= $time_notif){
+                $time_notif = time()+15;
+                f("bot_kirim_perintah")("sendMessage",[
+                    "chat_id"=>f("get_config")('bot_admins')[0],
+                    "text"=>(time()-$time1)." detik berlalu.",
+                ]);
+            }
             if($loopcount > 9999999){
                 f("bot_kirim_perintah")("sendMessage",[
                     "chat_id"=>f("get_config")('bot_admins')[0],
