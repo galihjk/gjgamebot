42d02a394638be7e2c854764ff53debda04fe0e8
Galih Jaya Kusumah <galih.kusumah@bpk.go.id>
2023-07-07 11:47:44 +0700

    server

--- srv_run_fdb8e0f27050d87f4faad843c4da5f3f.php
+++ srv_run_fdb8e0f27050d87f4faad843c4da5f3f.php
@@ -1,6 +1,6 @@
 <?php
 include("init.php");
-$loopcount = 0;
+
 $time1 = time();
 $time_notif = time()+15;
 $srvcode = md5(date("mdHis").rand(0,100));
@@ -9,33 +9,57 @@ f("data_save")("servercode", $srvcode);
 //     "chat_id"=>f("get_config")('bot_admins')[0],
 //     "text"=>"Server Started: $srvcode",
 // ]);
+$offset = f("data_load")("get_updates_offset",0);
+$jenis_update = [
+    "message",
+    // "callback_query",
+];
 while(true){
     $srvcode_current = f("data_load")("servercode", "");
     if($srvcode_current != $srvcode){
-        f("bot_kirim_perintah")("sendMessage",[
-            "chat_id"=>f("get_config")('bot_admins')[0],
-            "text"=>"Server stopped. (srvcode_current=$srvcode_current, srvcode=$srvcode)",
-        ]);
-        return "SERVER STOPPED";
+        // f("bot_kirim_perintah")("sendMessage",[
+        //     "chat_id"=>f("get_config")('bot_admins')[0],
+        //     "text"=>"Server stopped. (srvcode_current=$srvcode_current, srvcode=$srvcode)",
+        // ]);
+        // return "SERVER STOPPED";
         break;
     }
-    $loopcount++;
+    
     if(time() >= $time_notif){
         $time_notif = time()+15;
-        f("bot_kirim_perintah")("sendMessage",[
-            "chat_id"=>f("get_config")('bot_admins')[0],
-            "text"=>(time()-$time1)." detik berlalu. $srvcode",
-        ]);
+        // f("bot_kirim_perintah")("sendMessage",[
+        //     "chat_id"=>f("get_config")('bot_admins')[0],
+        //     "text"=>(time()-$time1)." detik berlalu. $srvcode",
+        // ]);
         if(time()-$time1 > 60){
             f("srv.get_without_wait")(f("get_config")("run_srv"));
         }
         f("data_save")("servertime", time());
     }
     if(time()-$time1 > 70){
-        f("bot_kirim_perintah")("sendMessage",[
-            "chat_id"=>f("get_config")('bot_admins')[0],
-            "text"=>"limit 70 sec.",
-        ]);
+        // f("bot_kirim_perintah")("sendMessage",[
+        //     "chat_id"=>f("get_config")('bot_admins')[0],
+        //     "text"=>"limit 70 sec.",
+        // ]);
         break;
+    }    
+
+    $getupdates = f("bot_kirim_perintah")("getUpdates",[
+        "offset"=>$offset,
+    ]);
+    if(!empty($getupdates["result"])){
+        $updates = $getupdates["result"];
+        foreach($updates as $update){
+            foreach($jenis_updates as $item_jenis){
+                if(!empty($update[$item_jenis])){
+                    f("handle_$item_jenis")($update[$item_jenis]);
+                    break;
+                }
+            }
+            file_put_contents("log/unhandled_LAST.txt", $update);
+            $offset = 1+$update["update_id"];
+        }
+        f("data_save")("get_updates_offset",$offset);
     }
+
 }
