8c450f0cd75383eec4caf577d359a2b64c635672
Galih Jaya Kusumah <galih.kusumah@bpk.go.id>
2023-08-16 05:58:07 +0700

    bugfix1

--- functions/bot/handle/botdata_functions.php
+++ functions/bot/handle/botdata_functions.php
@@ -2,11 +2,6 @@
 function bot__handle__botdata_functions($botdata, $functions){
     $result = false;
     foreach($functions as $function){
-        $filename = "functions/$function.php";
-        if(!file_exists($filename)){
-            file_put_contents("log/f_handle_not_exist_".date("Y-m-d-H-i").".txt", $function);
-            return false;
-        }
         return f($function)($botdata);
     }
     return false;
--- functions/str/parse_cmd.php
+++ functions/str/parse_cmd.php
@@ -7,7 +7,7 @@ function str__parse_cmd($str){
         $command = $explode[0];
         $command = str_replace('@'.f("get_config")("botuname"), '', $command);
         $command = substr($command, 1);
-        if(count($explode > 1)){
+        if(count($explode) > 1){
             $query = substr($str,1+strlen($explode[0]));
         }
         
