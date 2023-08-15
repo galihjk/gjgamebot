adf293f901d1169870b7f7b0fb4c3258e9fd4cf9
Galih Jaya Kusumah <galih.kusumah@bpk.go.id>
2023-08-16 06:45:42 +0700

    bugfix2

--- functions/bot/handle/botdata_functions.php
+++ functions/bot/handle/botdata_functions.php
@@ -2,7 +2,7 @@
 function bot__handle__botdata_functions($botdata, $functions){
     $result = false;
     foreach($functions as $function){
-        return f($function)($botdata);
+        if(f($function)) return f($function)($botdata);
     }
     return false;
 }
