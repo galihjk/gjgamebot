624990fe0244c322b84ac3a2bf192b9b1b48ae9a
Galih Jaya Kusumah <galih.kusumah@bpk.go.id>
2023-08-18 14:03:58 +0700

    srv stop

--- srv_stop.php
+++ srv_stop.php
@@ -1,7 +1,7 @@
 <?php
 include("init.php");
-$srvcode = md5(date("mdHis").rand(0,100));
-f("data.save")("servercode", $srvcode);
+// $srvcode = md5(date("mdHis").rand(0,100));
+f("data.save")("servercode", "STOPPED");
 echo "OK";
 // f("bot.kirim_perintah")("sendMessage",[
 //     "chat_id"=>f("get_config")('bot_admins')[0],
